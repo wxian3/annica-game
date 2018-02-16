1201e6132d54bb34d8ceff06ca0a1d586c48f4a6
Wenqi Xian <wxian3@gatech.edu>
2018-02-16 10:43:02 -0500

    Assests and project settings

---
+++ Assets/AppContent/Scripts/Box.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+	AudioSource coinSound;
+	public AudioClip coinClip;
+	public GameObject coinModel;
+	private Shader shader;
+	public Renderer rend;
+	bool boxHit = false;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		coinModel.SetActive (false);
+		coinSound = GetComponent<AudioSource> ();
+		shader = Shader.Find ("02 - Default");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 0, 45) * Time.deltaTime);
+	
+	}
+
+	void OnCollisionEnter(Collision info) {
+		if (info.gameObject.name == "protagonist" && boxHit == false) {
+			Debug.Log ("box is hit");
+			//coinSound.clip = coinClip;
+			//coinSound.Play ();
+			//coinModel.SetActive (true);
+			//Destroy (coinModel, 1.5f);
+
+			Color blackColor = new Color (0, 0, 0, 1.0f);
+			gameObject.GetComponent<Renderer> ().material.color = blackColor;
+			boxHit = true;
+			Component halo = GetComponent("Halo"); 
+			halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+		}
+		GameObject.Find ("Game").GetComponent<game>().Absorb ();
+	}
+}
---
+++ Assets/bridge_control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bridge_control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void rotate() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
