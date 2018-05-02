3d7390878718f5e281090e07de3f8e45bcca15c3
Danny Zhang <zhangdanny96@gmail.com>
2018-05-01 20:14:25 -0400

    enhanced environment

--- Assets/AppContent/Scripts/Bridge.cs
+++ Assets/AppContent/Scripts/Bridge.cs
@@ -20,7 +20,7 @@ public class Bridge : MonoBehaviour {
 //		}
 		if (rotating) {
 			Vector3 rotate_to = new Vector3 (0, 90, 0);
-			Vector3 transform_to = new Vector3 (-6, 0, 19);
+			Vector3 transform_to = new Vector3 (-6, 2.4f, 19);
 			if (Vector3.Distance (transform.eulerAngles, rotate_to) > 0.01f) {
 				transform.position = Vector3.Lerp (transform.position, transform_to, Time.deltaTime);
 				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, rotate_to, Time.deltaTime );
---
+++ Assets/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public int height;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin((Time.time * height) / 2), transform.position.z);
+
+        if (transform.position.y > 1.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.y < -1.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+	}
+}
